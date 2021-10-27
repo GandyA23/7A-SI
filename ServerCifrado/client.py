3457be9036366c7bec80cdfad2dfce715e981fa6
GandyA23 <avila.gandy@gmail.com>
2021-10-27 09:57:44 -0500

    Chat que usa el cifrado

---
+++ ServerCifrado/client.py
@@ -0,0 +1,57 @@
+"""
+Gandy Esau Avila Estrada
+7° A
+Cliente
+"""
+import socket
+import time
+import sys
+
+sys.path.append('../CifradoSimetrico')
+
+from Cryptography.Symmetric import Symmetric
+
+# Para cifrar los mensajes
+crypto = Symmetric()
+
+print('..: Cliente :..\n')
+
+if crypto.allItsFine() :
+    time.sleep(1)
+    cliente_socket = socket.socket()
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+
+    print(host, '({})'.format(ip))
+
+    nombre_usuario = input("Indique su nombre de usuario: ")
+    ip_host = input("¿A qué IP se conectará?: ")
+    puerto = input("¿A qué puerto se conectará?: ")
+
+    time.sleep(1)
+    cliente_socket.connect((str(ip_host), int(puerto)))
+    print("¡Conexión establecida! Inicia la comunicación.\n")
+
+    cliente_socket.send(nombre_usuario.encode())
+    server_name = cliente_socket.recv(1024)
+    server_name = server_name.decode()
+
+    print('Escriba salir para finalizar comunicación.')
+    while True:
+        mensaje = cliente_socket.recv(1024)
+        mensaje = mensaje.decode()
+        mensaje = crypto.decode(mensaje)
+        print(server_name," dice: ", mensaje)
+        mensaje = input(str("Yo: "))
+        if mensaje == "salir":
+            mensaje = "se desconectó."
+            cliente_socket.send(mensaje.encode())
+            print("\n")
+            break
+
+        ## Cifra el mensaje del cliente 
+        mensaje = crypto.encode(mensaje)
+        
+        cliente_socket.send(mensaje.encode())
+else:
+    print('Error en la configuración, lee los mensajes de error')
---
+++ ServerCifrado/server.py
@@ -0,0 +1,59 @@
+"""
+Gandy Esau Avila Estrada
+7° A
+Servidor 
+"""
+
+import socket
+import time
+import sys
+
+sys.path.append('../CifradoSimetrico')
+
+from Cryptography.Symmetric import Symmetric
+
+time.sleep(1)
+
+crypto = Symmetric()
+
+if crypto.allItsFine() :
+    print("..: Servidor activo :..\n")
+    socket_server = socket.socket()
+    socket_server.bind(('localhost', 8000))
+
+    nombre_servidor = input("Ingrese el nombre del servidor: ")
+    socket_server.listen(1)
+
+    print("Esperando conexiones")
+    connection, addr = socket_server.accept()
+
+    print("Conexión recibida de ", addr[0], "(", addr[1], ")\n")
+    print("Conexión establecida. conectado de: {}, ({})".format(addr[0], addr[0]))
+
+    cliente = connection.recv(1024)
+    cliente = cliente.decode()
+    print(cliente + " se conectó.")
+
+    print("Teclee salir para desconectarse")
+    connection.send(nombre_servidor.encode())
+
+    while True:
+        mensaje = input('Yo: ')
+        
+        if mensaje == 'salir':
+            mensaje = 'Cerrando conexión.'
+            connection.send(mensaje.encode())
+            print("\n")
+            break
+
+        ## Cifra el mensaje del servidor 
+        mensaje = crypto.encode(mensaje)
+
+        connection.send(mensaje.encode())
+
+        mensaje = connection.recv(1024)
+        mensaje = mensaje.decode()
+        mensaje = crypto.decode(mensaje)
+        print(cliente, ' dice :', mensaje)
+else:
+    print('Error en la configuración, lee los mensajes de error')
